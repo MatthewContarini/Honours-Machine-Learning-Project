3469db227156c3daf2c504a2bded3b0925b9d0ce
MatthewContarini <90013765+MatthewContarini@users.noreply.github.com>
2023-01-10 09:52:44 +1000

    Processed Data Files Upload
    All media concentrations have been stored in sperate .csv files and should now be relatively easy to call any media concentration from colab.

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,5 @@
 from growth_wells import GrowthWells
-from PCA import *
-from KPCA import *
 from data_to_dictionary import *
-from data_visualisation import *
 
 # Import Settings
 ians_summer_data_mutants = ["RIF2_B5", "RIF2_F4", "RIF3_1B10", "RIF4_1E6"]
@@ -35,39 +32,46 @@ alicias_honours_mutants = [
     "MG1655",
 ]
 data_specifciation = {
-    "ians_summer_data": (
-        "Python/data/csv_data/Ians_Summer_01_18_to_01_28_Data.csv",
-        ians_summer_data_mutants,
-    ),
+    # "ians_summer_data": (
+    #     "data/csv_data/Ians_Summer_01_18_to_01_28_Data.csv",
+    #     ians_summer_data_mutants,
+    # ),
     "alicias_new_data": (
-        "Python/data/csv_data/Alicias_New_Data.csv",
+        "data/csv_data/Alicias_New_Data.csv",
         alicias_mutants_new,
     ),
-    "alicias_honours_data": (
-        "Python/data/csv_data/Alicias_Honours_Data.csv",
-        alicias_honours_mutants,
-    ),
+    # "alicias_honours_data": (
+    #     "data/csv_data/Alicias_Honours_Data.csv",
+    #     alicias_honours_mutants,
+    # ),
 }
-pickle_file_path = "Python/data/pickled_data/growth_well_dictionary.pickle"
+pickle_file_path = "data/pickled_data/growth_well_dictionary.pickle"
 
 data = GrowthWells()
 data.load_growth_data(
-    data_file_specification=pickle_file_path,
-    from_pickle=True,
-    verbose=False,
+    data_file_specification=data_specifciation,
+    from_pickle=False,
+    verbose=True,
     save_as_pickle=False,
 )
 
 data.set_start_time(0)
-data.align_times(15)
+data.align_times(3)
 data.truncate(1380)
 
 # print(data.get_growth_well(("mainMutants", 1, "B", 5)).get_relative_growth_rates())
 
-growth_wells_dataframe = data.generate_dataframe(data_type="ABS_OD", safe=True)
+growth_wells_dataframe = data.generate_dataframe(data_type="RGR", safe=True)
 
+concnetrations = [0.0005, 0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.008, 0.01]
+names = ['0-0005', '0-001', '0-002', '0-003', '0-004', '0-005', '0-006', '0-008', '0-01']
+j = 0
+for i in concnetrations:
+    growth_wells_dataframe_p = growth_wells_dataframe.loc[growth_wells_dataframe['Media Concentration'] == i]
+    print(growth_wells_dataframe_p.head())
 
-# growth_wells_dataframe.to_csv("growth_wells_dataframe", encoding="utf-8", index=False)
+    growth_wells_dataframe_p.to_csv(f"../data_for_colab/proccessed_growth_data/3Min_{names[j]}MC_RGR", encoding="utf-8", index=False)
+    j += 1
 
 # wrapper = PCA_for_growth_well_data(
 #     growth_wells_dataframe, ["Media Concentration", "Strain"], list(range(0, 1380, 15))
--- Python/unsupervised_algorithm.py
+++
@@ -1,6 +0,0 @@
-class Unsupervised_Algorithm:
-    def __init__(self, dataframe, target_names, feature_names):
-        self.data_dataframe = dataframe
-        self.features = dataframe.loc[:, feature_names]  # Get the features.
-        self.targets = dataframe.loc[:, target_names]  # Get the targets.
-        
